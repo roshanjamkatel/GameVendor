c709d85509c1b1776b1b7db829f1ad3fa40e5335
Roshan Jamkatel <roshan.jamkatel@gmail.com>
2021-04-09 17:10:36 -0500

    Spring boot setup
    Added files for Spring Boot Initalization.

---
+++ src/main/java/com/example/GameVendor/GameVendorApplication.java
@@ -0,0 +1,13 @@
+package com.example.GameVendor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameVendorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameVendorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group7ooec/gamevendor/GamevendorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group7ooec.gamevendor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamevendorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
